e6a51f987e4dd323bab0cf98d386bc646a486b83
ilya <ilya.vas@outlook.com>
2023-11-10 21:39:36 +0200

    decimal fields fix

--- seashells/concept/models.py
+++ seashells/concept/models.py
@@ -4,8 +4,8 @@ from cities_light.models import Country
 
 class CountryTaxesConcept(models.Model):
     name = models.CharField(max_length=100)
-    rate_percentage = models.DecimalField()
-    rate_fixed = models.DecimalField()
+    rate_percentage = models.DecimalField(max_digits=20, decimal_places=8)
+    rate_fixed = models.DecimalField(max_digits=20, decimal_places=8)
     rate_formula = models.CharField(max_length=100)
     country = models.ForeignKey(Country, on_delete=models.SET_NULL, null=True, blank=True)
 
